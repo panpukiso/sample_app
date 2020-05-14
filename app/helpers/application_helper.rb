36cce9fcfcf30f27bd32c1590dc631a57304dc51
Tabata Tsukasa <tpanpukint5625@gmail.com>
2020-05-14 23:11:56 +0000

    finish ch4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title= '')
+    basic_title = "Ruby on Rails Tutorial"
+    if page_title.empty?
+      basic_title
+    else
+      page_title + " | " +  basic_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial"
+    assert_select "title", "Ruby on Rails Tutorial"
   end
 
   test "should get help" do
