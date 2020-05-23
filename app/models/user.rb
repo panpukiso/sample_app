a661beb71b33ee09850d9f354a7ef21147782e4c
Tabata Tsukasa <tpanpukint5625@gmail.com>
2020-05-23 03:18:59 +0000

    partial resolve

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@ class User < ApplicationRecord
                   uniqueness: true
   has_secure_password
   validates :password, presence: true,
-                         length: { minimum: 6 }
+                         length: { minimum: 6 },
+                         allow_nil: true
                          
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
