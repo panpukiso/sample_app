fdac76fe8c0513a585251503ffc26939e37f513e
Tabata Tsukasa <tpanpukint5625@gmail.com>
2020-06-01 07:32:29 +0000

    finish ch12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
   
   get '/home'    => 'static_pages#home'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
